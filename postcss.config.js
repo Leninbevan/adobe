c37c06bdb2bc1e1a977c5b9a9dd0faf77fbb6437
lenin_bevan <charleslenin431@gmail.com>
2025-02-04 19:19:13 +0530

    removed the unwanted files

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("postcss-varfallback"),
-    require("postcss-dropunusedvars")({ fix: true }),
-    require("cssnano"),
-    require("postcss-remove-unused-css")({path: "./hlx_statics"}),
-  ],
-};
